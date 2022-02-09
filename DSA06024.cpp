47c9aef74af2a03c6ec433477c5704377fd54dc2
xfeex1111 <73334675+xfeex1111@users.noreply.github.com>
2022-02-09 19:43:03 +0700

    Sắp xếp chọn

---
+++ DSA06024.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+using ll = long long;
+
+#define tests() int t; cin >> t; while (t--)
+#define For(i,a,b) for(int i=a; i<b; i++)
+#define FOR(i,a,b) for(int i=a; i<=b; i++)
+
+const ll MOD = 1e9+7;
+int n, cnt, k;
+
+void in(int a[]){
+    For(i,0,n) cout << a[i] << " ";
+    cout << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> n;
+    int a[105];
+    For(i,0,n) cin >> a[i];
+    For(i,0,n-1){
+        int key = i;
+        For(j,i+1,n){
+            if(a[j] < a[key]) key = j;
+        }
+        swap(a[i], a[key]);
+        cout << "Buoc " << i+1 << ": ";
+        in(a);
+    }
+}   
+
