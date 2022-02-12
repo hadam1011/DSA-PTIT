f22c7e9e788c8b2ccd540a49bba567f9d9c4674f
xfeex1111 <73334675+xfeex1111@users.noreply.github.com>
2022-02-12 22:13:56 +0700

    Create DSA01017.cpp

---
+++ DSA01017.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+#define tests() int t; cin >> t; while (t--)
+#define For(i,a,b) for(int i=a; i<b; i++)
+#define FOR(i,a,b) for(int i=a; i<=b; i++)
+
+const ll MOD = 1e9+7;
+int n, cnt, k, check[15], a[1001];
+string s;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    tests(){
+        cin >> s;
+        string res = "";
+        res += s[0];
+        For(i,1,s.size()){
+            if(s[i] == s[i-1]) res += "0";
+            else res += "1";
+        }
+        cout << res << endl;
+    }   
+}
