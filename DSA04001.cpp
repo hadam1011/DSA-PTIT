f6773341f9c250f44fca65de21f0b677093a7c1d
xfeex1111 <73334675+xfeex1111@users.noreply.github.com>
2022-02-21 10:37:16 +0700

    Lũy thừa

---
+++ DSA04001.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define tests() int t; cin >> t; while (t--)
+#define For(i,a,b) for(int i=a; i<b; i++)
+#define FOR(i,a,b) for(int i=a; i<=b; i++)
+
+typedef long long ll;
+const ll MOD = 1e9+7;
+
+ll Solve(int n, int k){
+    if(k == 0) return 1;
+    ll x = Solve(n, k/2) % MOD; 
+    ll ans = (x * x) % MOD;
+    if(k % 2) return (ans * n) % MOD;
+    return ans % MOD;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    tests(){
+        int n, k;
+        cin >> n >> k;
+        cout << Solve(n,k) << endl;
+    }
+}
