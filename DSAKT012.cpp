91f8a34f15d55160edcf2b1ddb52e12adfe0a38d
xfeex1111 <73334675+xfeex1111@users.noreply.github.com>
2022-02-22 15:46:20 +0700

    Đổi tiền

---
+++ DSAKT012.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define tests() int t; cin >> t; while (t--)
+#define For(i,a,b) for(int i=a; i<b; i++)
+#define FOR(i,a,b) for(int i=a; i<=b; i++)
+
+typedef long long ll;
+const ll MOD = 1e9+7;
+int n, s, a[50], kq = INT_MAX;
+
+void Try(int i, int sum, int x){
+    if(sum > s || x > kq) return;
+    if(i == n){
+        if(sum == s) kq = min(kq, x);
+        return;
+    }
+    Try(i+1, sum, x);
+    Try(i+1, sum+a[i], x+1);
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);  
+    cin >> n >> s;
+    For(i,0,n) cin >> a[i];
+    Try(0,0,0);
+    if(kq == INT_MAX) cout << -1;
+    else cout << kq;
+}
