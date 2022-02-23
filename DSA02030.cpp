223add32a0a0951f3e8907e01d9353e9e9cbb3b6
xfeex1111 <73334675+xfeex1111@users.noreply.github.com>
2022-02-23 22:51:12 +0700

    Liệt kê xâu kí tự

---
+++ DSA02030.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define tests() int t; cin >> t; while (t--)
+#define For(i,a,b) for(int i=a; i<b; i++)
+#define FOR(i,a,b) for(int i=a; i<=b; i++)
+
+typedef long long ll;
+
+int n, k, a[50], check[50], spt = 0; 
+string s = "";
+
+void in(){
+    For(i,0,s.size()-1){
+        if(s[i] > s[i+1]) return;
+    }
+    cout << s << endl;
+}
+
+void Try(){
+    FOR(j,1,n){
+        s += char('A' + j - 1);
+        spt++;
+        if(spt == k) in();
+        else Try();
+        s.erase(s.size()-1); 
+        spt--;
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    char x;
+    cin >> x >> k;
+    n = x - 'A' + 1;
+    Try(1);
+}
