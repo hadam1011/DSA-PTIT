a3f66fe37aea36e5d0abdb818a811234037ca343
Đàm <73334675+xfeex1111@users.noreply.github.com>
2022-03-10 10:27:57 +0700

    Sắp đặt xâu kí tự 2

---
+++ DSA03013.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define tests() int t; cin >> t; while (t--)
+#define For(i,a,b) for(int i=a; i<b; i++)
+#define FOR(i,a,b) for(int i=a; i<=b; i++)
+#define FORD(i,a,b) for(int i=a; i>=b; i--)
+#define ForD(i,a,b) for(int i=a; i>b; i--)
+
+typedef long long ll;
+
+int n, k, a[26];
+int MOD = 1e9 + 7;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    tests(){
+        string s;
+        cin >> k >> s;
+        int len = s.size();
+        memset(a, 0, sizeof(a));
+        For(i,0,len) a[s[i]-'A']++;
+        sort(a, a+26, greater<int>());
+        if((a[0]-1) * (k-1) > len - a[0]) cout << -1 << endl;
+        else cout << 1 << endl;
+    }
+}
