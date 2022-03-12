d25808c0df865119c2e9791de8ed9868db75b5e4
Đàm <73334675+xfeex1111@users.noreply.github.com>
2022-03-12 22:13:36 +0700

    DFS trên đồ thị có hướng

---
+++ DSA09022.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define tests() int t; cin >> t; cin.ignore(); while(t--)
+#define For(i,a,b) for(int i=a; i<b; i++)
+#define FOR(i,a,b) for(int i=a; i<=b; i++)
+#define FORD(i,a,b) for(int i=a; i>=b; i--)
+#define ForD(i,a,b) for(int i=a; i>b; i--)
+
+typedef long long ll;
+
+int n, m, u, v, start;
+int visited[1001], a[1001][1001];
+int MOD = 1e9 + 7;
+
+void DFS(int i){
+    visited[i] = 1;
+    cout << i << " ";
+    FOR(j,1,n){
+        if(!visited[j] && a[i][j]) DFS(j);
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    tests(){
+        memset(a, 0, sizeof(a));
+        memset(visited, 0, sizeof(visited));
+        cin >> n >> m >> start;
+        For(i,0,m){
+            cin >> u >> v;
+            a[u][v] = 1;
+        }
+        DFS(start);
+        cout << endl;
+    }   
+}
