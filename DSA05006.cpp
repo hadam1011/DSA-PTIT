3c42d02f775e1b0bb40a7099bc8c27cf45efb514
Đàm <73334675+xfeex1111@users.noreply.github.com>
2022-03-15 09:35:35 +0700

    Tổng lớn nhất của dãy con tăng dần

---
+++ DSA05006.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define tests() int t; cin >> t; cin.ignore(); while(t--)
+#define For(i,a,b) for(int i=a; i<b; i++)
+#define FOR(i,a,b) for(int i=a; i<=b; i++)
+#define FORD(i,a,b) for(int i=a; i>=b; i--)
+#define ForD(i,a,b) for(int i=a; i>b; i--)
+
+typedef long long ll;
+
+int n, x;
+int res[1001], a[1001];
+int MOD = 1e9 + 7;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    tests(){
+        cin >> n;
+        For(i,0,n) cin >> a[i];
+        int kq = 0;
+        For(i,0,n){
+            x = 0; 
+            res[i] = a[i];
+            For(j,0,i){
+                if(a[i] > a[j]) x = max(x, res[j]);
+            }
+            res[i] += x;
+            kq = max(kq, res[i]);
+        }
+        cout << kq << endl;
+    }
+}
