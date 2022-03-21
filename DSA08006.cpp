cd6a08156a37fe6e9c0992b36eeb4a6f4a0c2206
Đàm <73334675+xfeex1111@users.noreply.github.com>
2022-03-21 15:32:55 +0700

    Số 0 và số 9

---
+++ DSA08006.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define tests() int t; cin >> t; cin.ignore(); while(t--)
+#define For(i,a,b) for(int i=a; i<b; i++)
+#define FOR(i,a,b) for(int i=a; i<=b; i++)
+#define FORD(i,a,b) for(int i=a; i>=b; i--)
+#define ForD(i,a,b) for(int i=a; i>b; i--)
+
+typedef long long ll;
+
+int n, k;
+int MOD = 1e9 + 7;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    tests(){
+        cin >> n;
+        queue<string> q;
+        q.push("9");
+        
+        while(1){
+            string x = q.front(); 
+            q.pop();
+            ll res = 0;
+            For(i,0,x.size()){
+                res = res*10 + (x[i] - '0');
+                res %= n;
+            }
+            if(res == 0){
+                cout << x << endl;
+                break;
+            }
+            q.push(x + "0");
+            q.push(x + "9");
+        }
+    }
+}
