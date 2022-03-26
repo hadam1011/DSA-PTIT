6d861e6ff19b4dd347d37d9582a9a05b239b5450
Đàm <73334675+xfeex1111@users.noreply.github.com>
2022-03-26 16:49:30 +0700

    Kiểm tra chu trình trên đồ thị vô hướng

---
+++ DSA09014.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define tests() int t; cin >> t; cin.ignore(); while(t--)
+#define For(i,a,b) for(int i=a; i<b; i++)
+#define FOR(i,a,b) for(int i=a; i<=b; i++)
+#define FORD(i,a,b) for(int i=a; i>=b; i--)
+#define ForD(i,a,b) for(int i=a; i>b; i--)
+
+typedef long long ll;
+
+int n, m;
+int MOD = 1e9 + 7;
+vector<int> adj[1000];
+int visited[1000];
+
+int DFS(int u, int previous){
+    visited[u] = 1;
+    for(int x : adj[u]){
+        if(!visited[x]) DFS(x, u);
+        else if(x != previous) return 1;
+    }
+    return 0;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    tests(){
+        memset(visited, 0, sizeof(visited));
+        int kt = 0;
+        cin >> n >> m;
+        while(m--){
+            int x, y;
+            cin >> x >> y;
+            adj[x].push_back(y);
+            adj[y].push_back(x);
+        }
+
+        FOR(i,1,n){
+            if(!visited[i]){
+                if(DFS(i, 0)){
+                    kt = 1;
+                    break;
+                }
+            }
+        }
+
+        if(kt) cout << "YES" << endl;
+        else cout << "NO" << endl;
+        FOR(i,1,1000) adj[i].clear();
+    }
+}
