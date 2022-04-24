b3d1e5d185cde3214eae35ffe9fc069afcf22916
Đàm <73334675+xfeex1111@users.noreply.github.com>
2022-04-24 21:02:23 +0700

    Nhịp chứng khoán

---
+++ DSA07028.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define tests() int t; cin >> t; while (t--)
+#define For(i,a,b) for(int i=a; i<b; i++)
+#define FOR(i,a,b) for(int i=a; i<=b; i++)
+#define FORD(i,a,b) for(int i=a; i>=b; i--)
+#define ForD(i,a,b) for(int i=a; i>b; i--)
+
+typedef long long ll;
+
+int n, m;
+int MOD = 1e9 + 7;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    tests(){
+        cin >> n;
+        int a[n], len[n];
+        For(i, 0, n) cin >> a[i];
+
+        stack<int> st;
+        st.push(0);
+        len[0] = 1;
+        cout << 1 << " ";
+
+        For(i, 1, n){
+            len[i] = 1;
+            if(a[i] < a[st.top()]){
+                cout << 1 << " ";
+            }else{
+                while(a[i] >= a[st.top()]){
+                    len[i] += len[st.top()];
+                    st.pop();
+                    if(st.empty()) break;
+                }
+
+                cout << len[i] << " ";
+            }
+            st.push(i);
+        }
+
+        cout << endl;
+    }
+}
